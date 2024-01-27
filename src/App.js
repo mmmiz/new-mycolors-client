5a6fcfadcc6f536429ec14593eb2e2f7468d0e21
mmmiz <miduho4soft@yahoo.co.jp>
2024-01-27 12:37:43 -0600

    added

--- src/App.js
+++ src/App.js
@@ -20,22 +20,22 @@ function App() {
   const {currentUser} = useContext(AuthContext);
 
   return (
-      // <Routes>
+      <Routes>
 
-      //   <Route path='/' element={<Layout />}>
-      //     <Route index element={<Home />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
         
-      //     <Route path="/login" element={<Login />} />
-      //     <Route path="/register" element={<Register />} />
-      //     <Route path="/profile" element={<Profile />} />
-      //     <Route path="/" element={<Register />} />
-      //     <Route path="/allcolors" element={<AllColors />} />
-      //     <Route path="/allcolors/:orderNumber" element={<DetailColors />} />
-      //     <Route path="/mycolors" element={<MyColors />} />
-      //     <Route path="/mylikes" element={<LikedColors />} />
-      //   </Route>
-      // </Routes>
-      <h1>unko</h1>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Register />} />
+          <Route path="/allcolors" element={<AllColors />} />
+          <Route path="/allcolors/:orderNumber" element={<DetailColors />} />
+          <Route path="/mycolors" element={<MyColors />} />
+          <Route path="/mylikes" element={<LikedColors />} />
+        </Route>
+      </Routes>
+      // <h1>unko</h1>
 
 
   );
